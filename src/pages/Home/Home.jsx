5ae777b3adc44540c7fc261c4790afea197dd9ab
NikkitoPlay <marcos.nikkito@gmail.com>
2024-06-08 12:03:06 -0400

    posts na home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,15 @@ import styles from "./Home.module.css";
 //hooks
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 //componentes
+import PostDetails from "../../components/PostDetails";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
-
+  const {documents: posts, loading} = useFetchDocuments('posts');
+  //console.log(posts)
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -28,7 +30,10 @@ const Home = () => {
         <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>Posts...</h1>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post)=>(
+          <PostDetails key={post.id} post={post}/>
+        ))}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
