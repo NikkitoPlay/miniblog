aac903d571df3bed7586a49598c3038e1cd3c024
NikkitoPlay <marcos.nikkito@gmail.com>
2024-06-10 14:39:12 -0400

    search params

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import CreatePost from "./pages/CreatePost/CreatePost";
+import Search from "./pages/Search/Search";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -49,6 +50,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
               <Route path="/login" element={!user ? <Login /> : <Navigate to='/'/>} />
               <Route path="/register" element={!user ? <Register /> : <Navigate to='/'/>} />
               <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to='/login'/>} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,11 +10,17 @@ import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import PostDetails from "../../components/PostDetails";
 
 const Home = () => {
+  const navigate = useNavigate();
+  
   const [query, setQuery] = useState("");
-  const {documents: posts, loading} = useFetchDocuments('posts');
+  const { documents: posts, loading } = useFetchDocuments("posts");
   //console.log(posts)
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
   };
 
   return (
@@ -31,13 +37,14 @@ const Home = () => {
       </form>
       <div>
         {loading && <p>Carregando...</p>}
-        {posts && posts.map((post)=>(
-          <PostDetails key={post.id} post={post}/>
-        ))}
+        {posts &&
+          posts.map((post) => <PostDetails key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link to='posts/create' className='btn'>Criar primeiro post</Link>
+            <Link to="posts/create" className="btn">
+              Criar primeiro post
+            </Link>
           </div>
         )}
       </div>
