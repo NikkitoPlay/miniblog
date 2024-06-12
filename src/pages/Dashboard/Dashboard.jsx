c05ad2f6971b41cc357af4a0e08d5cdf4d1394bb
NikkitoPlay <marcos.nikkito@gmail.com>
2024-06-12 11:49:06 -0400

    dashboard no posts

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,34 @@
-import styles from './Dashboard.module.css';
+import styles from "./Dashboard.module.css";
+
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
+  const {user} = useAuthValue();
+  const uid = user.uid;
+
+
+  //posts do usuario
+  const posts = []
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencia os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts :(</p>
+          <Link to='/posts/create' className="btn">Criar primiero post</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
